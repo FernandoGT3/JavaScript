1aad744bdbe10c59c2cc2bb3eb9b8003d8f233fa
Luiz Moloni <luiz.moloni@gmail.com>
2022-03-19 16:58:56 -0300

    Arquivos JavaScript
    Express Static -> Adicionando conteúdos estáticos, que são carregados no front-end

---
+++ Seção-Node/Aula-11/server.js
@@ -0,0 +1,20 @@
+//Arquivos Estáticos -> Arquivos que são Carregados No front-End
+
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+const path = require('path');
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.resolve(__dirname, "public")));
+
+app.set('views', path.resolve(__dirname, "src", "views"));
+app.set('view engine', 'ejs');
+
+app.use(routes);
+
+app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000/');
+    console.log('Servidor Executando na Porta 3000');
+});
